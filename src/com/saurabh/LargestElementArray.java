93ed734a4602892f5acd38f2385a26ddb7b12aa0
SaurabhAgarwal207 <mcasaurabhagarwal@gmail.com>
2024-01-01 19:26:46 +0530

    Largest Element Array

---
+++ src/com/saurabh/LargestElementArray.java
@@ -0,0 +1,14 @@
+package com.saurabh;
+
+public class LargestElementArray {
+    public static void main(String[] args) {
+        int arr[]={10,40,20,60,80,50};
+        int max= Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        System.out.println("Largest element in array is :"+max);
+    }
+}
