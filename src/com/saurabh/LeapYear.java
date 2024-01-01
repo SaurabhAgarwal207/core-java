73538e2d361a874178aa68d4ef27428f090c2b21
SaurabhAgarwal207 <mcasaurabhagarwal@gmail.com>
2024-01-01 10:00:44 +0530

    leap year

---
+++ src/com/saurabh/LeapYear.java
@@ -0,0 +1,14 @@
+package com.saurabh;
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int year=scan.nextInt();
+        if((year%4==0 && year%100!=0) || year %400==0)
+            System.out.println("Leap year");
+        else
+            System.out.println("Not leap year");
+    }
+}
