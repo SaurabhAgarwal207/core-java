075eb2a8822db475927be62eebc2bd2e6dedf5f8
SaurabhAgarwal207 <mcasaurabhagarwal@gmail.com>
2024-01-02 21:31:10 +0530

    Count one particular word in string

---
+++ src/com/saurabh/CountParticularWord.java
@@ -0,0 +1,15 @@
+package com.saurabh;
+
+public class CountParticularWord {
+    public static void main(String[] args) {
+        String str="my name is my my find it";
+        String toFind="my";
+        String arr[]=str.split(" ");
+        int count=0;
+        for(String s:arr){
+            if(s.equals(toFind))
+                count++;
+        }
+        System.out.println("Total occurrences of word "+toFind+" is : "+count);
+    }
+}
