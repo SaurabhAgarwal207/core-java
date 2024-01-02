51102f704f68301673a25cf644af62359369bc11
SaurabhAgarwal207 <mcasaurabhagarwal@gmail.com>
2024-01-02 18:39:20 +0530

    Duplicate Character in a String

---
+++ src/com/saurabh/DuplicateCharacterString.java
@@ -0,0 +1,20 @@
+package com.saurabh;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DuplicateCharacterString {
+    public static void main(String[] args) {
+        String str="abnnddcce";
+
+        HashMap<Character,Integer> strMap = new HashMap<Character,Integer>();
+        for(char ch:str.toCharArray()){
+            strMap.put(ch,strMap.getOrDefault(ch,0)+1);
+        }
+
+        for(Map.Entry<Character,Integer> entry : strMap.entrySet()){
+            if(entry.getValue()>1)
+                System.out.println(entry.getKey());
+        }
+    }
+}
