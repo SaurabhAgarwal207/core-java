1acb45dc6fde036c8daf8827c813eed94c396b08
SaurabhAgarwal207 <mcasaurabhagarwal@gmail.com>
2024-01-05 00:00:06 +0530

    Count occurrence of substring in a string

---
+++ src/com/saurabh/OccurenceOfSubstringInString.java
@@ -0,0 +1,18 @@
+package com.saurabh;
+
+public class OccurenceOfSubstringInString {
+    public static void main(String[] args) {
+        String str="my name is my jojo my uncle";
+        String sub="my";
+        int count=0,index=0;
+        while(index!=-1) {
+            index = str.indexOf(sub, index);
+            if(index!=-1) {
+                count++;
+                index += sub.length();
+            }
+        }
+        System.out.println("Occurence of "+sub+" is :"+count);
+
+    }
+}
