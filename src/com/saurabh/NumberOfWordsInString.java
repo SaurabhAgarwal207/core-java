ad0f73c3f9150fe5517fb7d168a0f73df3ac7bbe
SaurabhAgarwal207 <mcasaurabhagarwal@gmail.com>
2024-01-04 21:35:08 +0530

    Number of words in a string

---
+++ src/com/saurabh/NumberOfWordsInString.java
@@ -0,0 +1,13 @@
+package com.saurabh;
+
+public class NumberOfWordsInString {
+    public static void main(String[] args) {
+        String str="my name is anthony gonsalves";
+        int count=0;
+        String arr[]=str.split(" ");
+        for(String s:arr){
+         count++;
+        }
+        System.out.println("Total words in string are "+count);
+    }
+}
---
+++ src/com/saurabh/PerfectPowerOf2.java
@@ -0,0 +1,27 @@
+package com.saurabh;
+
+public class PerfectPowerOf2 {
+    public static void main(String[] args) {
+        int num = 1;
+        boolean flag = false;
+        while (num >1) {
+            if (num % 2 == 0) {
+                num = num / 2;
+            } else {
+                flag = true;
+                break;
+            }
+        }
+        if(flag==false)
+            System.out.println("Number is perfect power of 2");
+        else{
+            System.out.println("Not perfect power");
+        }
+    }
+
+
+    {
+
+    }
+}
+
