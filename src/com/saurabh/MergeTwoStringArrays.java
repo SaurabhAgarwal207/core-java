700cf179a8b467e424dc8566677daedd0ed7a6be
SaurabhAgarwal207 <mcasaurabhagarwal@gmail.com>
2024-01-05 23:48:36 +0530

    MergeTwoStringArrays

---
+++ src/com/saurabh/MergeTwoStringArrays.java
@@ -0,0 +1,21 @@
+package com.saurabh;
+
+public class MergeTwoStringArrays {
+    public static void main(String[] args) {
+        String arr1[]= {"abc","def"};
+        String arr2[]= {"ghi","jkl"};
+        String arr3[] = new String[arr1.length+arr2.length];
+        int i=0,j=0;
+        while(i<arr1.length){
+            arr3[i]=arr1[i];
+            i++;
+        }
+        while(i<arr3.length){
+            arr3[i]=arr2[j];
+            i++;
+            j++;
+        }
+        for(String s: arr3)
+            System.out.print(s+" ");
+    }
+}
