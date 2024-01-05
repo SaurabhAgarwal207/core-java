461698a53e8f6bd9379f7f967fcf360fd80c90d8
SaurabhAgarwal207 <mcasaurabhagarwal@gmail.com>
2024-01-06 00:05:17 +0530

    Remove duplicate from String

---
+++ src/com/saurabh/RemoveDuplicatesFromString.java
@@ -0,0 +1,18 @@
+package com.saurabh;
+
+
+import java.util.LinkedHashSet;
+
+public class RemoveDuplicatesFromString {
+    public static void main(String[] args) {
+        String str= "my name is my saurabh";
+        String arr[]=str.split(" ");
+        LinkedHashSet<String> hs =new LinkedHashSet<>();
+        for(String s: arr){
+            hs.add(s);
+        }
+       // String arr2[]= hs.stream().toArray(String[] :: new);
+        for(String p: hs)
+            System.out.print(p+" ");
+    }
+}
