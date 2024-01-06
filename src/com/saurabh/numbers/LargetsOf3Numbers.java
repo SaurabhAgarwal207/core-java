443c184f31bf992c54708d8c9fbe347aa5fca61a
SaurabhAgarwal207 <mcasaurabhagarwal@gmail.com>
2024-01-07 01:32:42 +0530

    Largets of 3 numbers

---
+++ src/com/saurabh/numbers/LargetsOf3Numbers.java
@@ -0,0 +1,8 @@
+package com.saurabh.numbers;
+
+public class LargetsOf3Numbers {
+    public static void main(String[] args) {
+        int a=20,b=130,c=40;
+        System.out.println(a>b?(a>c?a:c):(b>c?b:c));
+    }
+}
